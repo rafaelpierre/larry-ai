138bdc7432b14b6cbbfb8b697d11a07067717fab
Rafael <rafael.pierre@gmail.com>
2023-11-11 17:54:51 +0100

    cov

--- src/tests/test_entrypoint.py
+++ src/tests/test_entrypoint.py
@@ -1,9 +1,15 @@
 from unittest import mock
+from larry.entrypoint import run
+
 
 @mock.patch("larry.entrypoint.main", mock.MagicMock())
 def test_entrypoint():
 
-    from larry.entrypoint import main
-
+    from larry.entrypoint import main, run
     main()
     main.assert_called_once()
+
+def test_run():
+
+    run(command = ["bash", "-c", "echo", "12345678910", "&&", "sleep 3", "&&", "echo 12345678910"])
+    pass
