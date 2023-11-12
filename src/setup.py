124fa1af816e58e00c79141d3af0af7875943213
Rafael <rafael.pierre@gmail.com>
2023-11-12 04:43:29 +0100

    description

--- src/setup.py
+++ src/setup.py
@@ -10,11 +10,13 @@ PACKAGE_REQUIREMENTS = [
     "openai==0.28.1"
 ]
 
-current_dir = Path(__file__).parent.parent
+current_dir = Path(__file__).parent
+long_description = (current_dir / "README.md").read_text()
 
 setup(
     name="larry-ai",
     packages=find_packages(),
+    long_description = long_description,
     include_package_data=True,
     setup_requires=["setuptools", "wheel"],
     install_requires=PACKAGE_REQUIREMENTS,
